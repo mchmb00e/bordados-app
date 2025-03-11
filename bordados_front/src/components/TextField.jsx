9a30ad22d83efae4a8f81fba3530581438f6677e
Miguel Chamorro <miguelchamorro912@gmail.com>
2025-03-11 04:57:38 -0300

    reestructure

--- bordados_front/src/components/TextField.jsx
+++
@@ -1,5 +0,0 @@
-function TextField(props) {
-    return <input onChange={(e) => props.onChange(e.target.value)} className={`form-control ${props.className}`} type={props.type} placeholder={props.placeholder}></input>
-}
-
-export default TextField;
--- bordados_front/src/components/Title.jsx
+++
@@ -1,5 +0,0 @@
-function Title(props) {
-    return <h1 className={`${ props.fontSize ? "fs-"+props.fontSize : "" }`}>Bordados<b className="text-primary">App</b></h1>
-}
-
-export default Title;
